34e42cd8a6ebe9e64238d47114675a6b73a898e3
Deepak <tanwer644d@gmail.com>
2023-06-05 18:21:35 +0530

    task

--- Task.js
+++ Task.js
@@ -2,26 +2,27 @@
 //Q2 find next grater elemet for circular array 
 //Q3 Impliment insertion sort using linked-list 
 const prompt = require('prompt-sync')();
-let arr=[1,3,2,4,2];
-let pre=[];
-let sum=0;
+let arr = [1, 3, 2, 4, 2];
+let pre = [];
+let sum = 0;
 arr.forEach(element => {
-    sum+=element
+    sum += element
     pre.push(sum);
 });
-console.log(pre);
-let res=[];
-let q=parseInt(prompt("Enter number of Query : "));
-while(q--)
-{
-    let startingIndex=parseInt(prompt("Enter starting Index : "));
-    let endingIndex=parseInt(prompt("Enter ending Index : "));
-
-    res.push(pre[endingIndex]-pre[startingIndex]);
-
+// console.log(pre);
+let res = [];
+let q = parseInt(prompt("Enter number of Query : "));
+let maxi = Number.MIN_SAFE_INTEGER;
+while (q--) {
+    let startingIndex = parseInt(prompt("Enter starting Index : "));
+    let endingIndex = parseInt(prompt("Enter ending Index : "));
+    let temp = startingIndex == 0 ? pre[endingIndex] : pre[endingIndex] - pre[startingIndex - 1];
+    maxi = temp > maxi ? temp : maxi;
+    res.push(temp);
 }
-
 console.log(res);
+console.log(maxi);
+
 
 
 
